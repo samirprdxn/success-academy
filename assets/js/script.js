42f09c14738ca876df299610391b47c4a20bc806
Amir <Amirs@prdxn.com>
2020-01-28 10:39:35 +0530

    Slider functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,9 @@
-/* Author: 
 
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// slick slider functionality
+$('.slider').slick({
+	infinite: true,
+	dots: true,
+	speed: 900,
+	// autoplay: true,
+	autoplayspeed: 200
+});
