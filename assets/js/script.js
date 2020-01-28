e622884419263f731fa30e57b53ea7ad5de2ced8
Amir <Amirs@prdxn.com>
2020-01-28 17:07:18 +0530

    Breakpoints added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
-// $(".tab").click(function(event){
-// 	event.preventDefault();
-// 	$tab = $(this);
-// 	if(!$tab.hasClass('active')) {
-// 		$(".tab").removeClass("active");
-// 		// $(".tab-content").fadeOut();
-// 		$tab.addClass('active');
-// 		// $tab.siblings("div").fadeIn();
-// 	}
-// });
+$('.hamburger').click(function(e) {
+	e.preventDefault();
+	$this = $(this);
+	$this.siblings('.nav_list').toggleClass('block');
+	$this.children('.hamburger_line1').toggleClass('ham1');
+	$this.children('.hamburger_line2').toggleClass('ham3');
+	$this.children('.hamburger_line3').toggleClass('ham2');
+});
 
 $(".tab").click(function(event){
 	event.preventDefault();
@@ -27,7 +25,46 @@ $(".tab").click(function(event){
 $('.slider').slick({
 	infinite: true,
 	dots: true,
-	speed: 900,
-	autoplay: true,
-	autoplayspeed: 200
+	speed: 900
+	,
+	responsive: 
+	[{
+		breakpoint: 995,
+		settings: {
+			dots: true
+		}
+	}]
+});
+
+var modal_content;
+$modal = $('.modal');
+$modal_container = $('.modal-container');
+$html = $('html');
+// display image modal
+$('.open-image').click(function(){
+	$html.addClass('remove-scroll');
+	$modal.addClass('show-modal');
+	modal_content = $("<img></img>");
+	modal_content.attr("src", $(this).attr('src'));
+	$modal_container.append(modal_content);
+});
+// modal closed when clicked on cross
+$('.close').click(function(){
+	$html.removeClass('remove-scroll');
+	$modal.removeClass('show-modal');
+	$modal_container.find('img').remove();
+	$modal_container.find('video').remove();
+});
+// avoid bubbling i.e don't close modal when clicked on modal container
+$modal_container.click(function( event ) {
+  	event.stopPropagation();
+});
+// modal closed when clicked on modal 
+$modal.click(function(e){
+	if ($(this).hasClass('show-modal')) {
+		$html.removeClass('remove-scroll');
+		$modal.removeClass('show-modal');
+		$modal_container.find('img').remove();
+		$modal_container.find('video').remove();
+	}
 });
